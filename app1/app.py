73873a0be1f70a5f95253378d63ea11ae5a1b34b
Nishad <nishad.hussain@gmail.com>
2025-07-21 22:28:47 +1000

    added prod deploy back

--- app1/app.py
+++ app1/app.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World App 1!'
 
-# Run the Flask application if this file is executed directly
+# Run the Flask application if this file is executed directly.
 if __name__ == '__main__':
     app.run(debug=True, port=8080,host='0.0.0.0')
